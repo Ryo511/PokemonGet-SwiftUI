6a2381e998bd11089ff54ec228addbcd58174f53
Ryo <134986643+Ryo511@users.noreply.github.com>
2024-04-23 14:33:18 +0900

    Initial Commit

---
+++ iOS201poke_33/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS201poke_33
+//
+//  Created by OLIVER LIAO on 2024/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS201poke_33/iOS201poke_33App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS201poke_33App.swift
+//  iOS201poke_33
+//
+//  Created by OLIVER LIAO on 2024/4/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS201poke_33App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
